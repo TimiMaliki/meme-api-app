24f82b6dcee31ab61d9d45350a21c9b8cd9fcb7d
TimiMaliki <malikitimi@gmail.com>
2024-01-31 15:26:51 +0100

    added:responsiveness

---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+/*===== MENU SHOW =====*/
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle', 'nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*==================== JOKE API ====================*/
+
+const jokeDisplay = document.querySelector(".joke")
+const btn = document.querySelector(".button-1")
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,political,racist,sexist,explicit&type=single"
+
+
+const getJoke = () => {
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data =>{
+          jokeDisplay.textContent = `${data.joke}`
+    })
+    // .catch(()=>{
+
+    // })
+    
+}
+getJoke()
+
+btn.addEventListener("click" , getJoke)
